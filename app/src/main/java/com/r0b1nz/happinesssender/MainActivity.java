2afae6a0f82a260845f3217b41f932b04c90055e
Robin Gautam <robin.gautam341@gmail.com>
2018-06-02 00:16:44 +0530

    A Working SMS Sender Prototype

---
+++ app/src/main/java/com/r0b1nz/happinesssender/MainActivity.java
@@ -0,0 +1,69 @@
+package com.r0b1nz.happinesssender;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        String textSMS;
+        int simID = 0; //0 = sim_1, 1 = sim_2
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        textSMS = "Hello! This is a simple text message.";
+        ArrayList<String> messageList = SmsManager.getDefault().divideMessage(textSMS);
+        String receiver = "9815540544";
+
+        if (messageList.size() > 1) {
+            SmsUtil.sendMultipartTextSMS(this, simID, receiver, null, messageList,
+                    null, null);
+        } else {
+            SmsUtil.sendSMS(this, simID, receiver, null, textSMS,
+                    null, null);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
